d50aa0137fa44cac8f804ea8c95142988421ca86
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-05-20 08:24:39 -0700

    completed assigned objectives and bonus objectives; grid resizing is functional; grid reset functional; opacity mode functional; and 'rainbow mode' is functional

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-buildGrid();
-setUpButton();
+var bRandomizeColor = false;
+var defaultColor = "black";
+var bOpacityMode = false;
 
+buildGrid();
+setUpResetButton();
+setUpRandomizeButton();
+setUpOpacityButton();
 
 
 function buildGrid(perSide=16){
@@ -9,38 +14,63 @@ function buildGrid(perSide=16){
 
     const total_grid_squares = per_side*per_side;    
     const divContainer = document.querySelector(".container");
-    // divContainer.style.width = (minGridSize * per_side + per_side*2) + "px";
 
     const SIZE_PERCENT = (divContainer.clientWidth/per_side)-2 + "px";
-    console.log(divContainer.clientWidth/(per_side+2))
-
-
 
     for (let j = 0; j < total_grid_squares; j++){
         let divNode = document.createElement('div');
 
-
         divNode.className = "grid-box"
-        
-        divNode.addEventListener("mouseover", () => {
-            divNode.style.backgroundColor = "blue";
-        });
-
-        // divNode.style.display = "flex"
-        // divNode.style.flex = "1";
-        divNode.style.border = 'solid black 1px';
+        divNode.style.border = 'solid white 1px';
         divNode.style.width = SIZE_PERCENT;
         divNode.style.height = SIZE_PERCENT;
         divNode.style.margin = "0";
+
+        if(bOpacityMode){
+            divNode.style.opacity = '0';
+
+            divNode.addEventListener("mouseover", () => {
+                divNode.style.backgroundColor = defaultColor;
+                divNode.style.borderColor = defaultColor;
+                let currentOpacity = +divNode.style.opacity;
+                divNode.style.opacity = `${currentOpacity + .1}`
+            });
+        }
+        
+        divNode.addEventListener("mouseover", () => {
+            if (bRandomizeColor){
+                let randomColor = getRandomColor();
+                divNode.style.backgroundColor = randomColor;
+                divNode.style.borderColor = randomColor;
+            } else {
+                divNode.style.backgroundColor = defaultColor;
+                divNode.style.borderColor = defaultColor;
+            }
+        });
+
         divContainer.appendChild(divNode);
         
     }
 }
 
-function setUpButton (){
-    const buttonRef = document.querySelector(".reset-button");
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+function getRandomColor(){
+    let red = randomIntFromInterval(0,255);
+    let blue = randomIntFromInterval(0,255);
+    let green = randomIntFromInterval(0,255);
+
+    colorInHex = '#' + red.toString(16) + blue.toString(16) + green.toString(16);
     
-    buttonRef.addEventListener('click', () => {
+    return colorInHex;
+}
+
+function setUpResetButton (){
+    const resetButton = document.querySelector(".reset-button");
+    
+    resetButton.addEventListener('click', () => {
         let newGridNumber = +prompt("Enter a number (1-100): ");
         
         //default to 16 if there is non-valid input
@@ -53,6 +83,29 @@ function setUpButton (){
         deleteOldGrid();
         buildGrid(newGridNumber);
     });
+}
+
+function setUpRandomizeButton () {
+    const randButton = document.querySelector('.randomize-button');
+
+    randButton.addEventListener('click', () =>{
+        if(bRandomizeColor) bRandomizeColor = false;
+        else bRandomizeColor = true;
+    });
+}
+
+function setUpOpacityButton () {
+    const opacityButton = document.querySelector('.opacity-button');
+
+    opacityButton.addEventListener('click', () =>{
+        if(bOpacityMode) bOpacityMode = false;
+        else bOpacityMode = true;
+
+        deleteOldGrid();
+        buildGrid();
+    });
+
+    
 }
 
 function deleteOldGrid (){
